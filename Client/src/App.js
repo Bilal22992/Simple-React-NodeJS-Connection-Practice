289865b885eaf649fe5678880d04e808224ad626
bilal22992 <haiderbilal22992@gmail.com>
2025-11-23 19:20:49 +0500

    task completed with get and post

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,30 @@ import { useState ,useEffect} from "react";
 function App() {
 
 const[data,setData]=useState([]);
+const [name ,setname]=useState("");
+
+
+
+function handleChange(event)
+{
+  const data = event.target.value;
+  setname(data);
+
+}
+
+function handleClick()
+{
+ 
+    fetch("http://localhost:5000/api",{
+      method:"POST",
+      headers:{"Content-Type": "application/json"},
+      body:JSON.stringify({ name })
+    }).then(response=> response.json()).then((result)=>{
+      setData(result.data);
+      setname("");
+    })
+  
+}
 
 useEffect(()=>
 {
@@ -18,14 +42,20 @@ return  ()=> clearInterval(interval)
 
   return (
     <div>
+
+
+      <input onChange={handleChange}></input>
+      <button onClick={handleClick}>ADD</button>
      {
+
+      
 
       data.length ===0 ?
      <p>Loading</p> :
-      data.map((names)=>
+      data.map((names,i)=>
       {
        
-        return <p>{names}</p>
+        return <p key={i}>{names}</p>
       })
      }
     </div>
--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,29 @@
 import express from "express";
 import cors from "cors";
+
+import fs from "fs";
 const app = express();
-const data=["moon","bilal","Saad"];
+
+const data = JSON.parse(fs.readFileSync("data.json"));
 app.use(cors({
     origin:"http://localhost:3000",
     methods:"GET"
-}))
+}));
 
+
+app.use(express.json());
 
 app.get("/api",(req,res)=>{
 
     res.send(data);
 })
+
+app.post("/api",(req,res)=>{
+    const {name}= req.body;
+    data.push(name);
+    res.send({success:true,data})
+})
+
 app.listen("5000",(req,res)=>{
 
     console.log("Server running on port 5000");
