4ba6cbe814472cf2089ac62744d33cf2c2fb2bf3
bilal22992 <haiderbilal22992@gmail.com>
2025-11-23 18:12:00 +0500

    backend connected to frontend successfully with automatic updates

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,33 @@
 
+import { useState ,useEffect} from "react";
 function App() {
+
+const[data,setData]=useState([]);
+
+useEffect(()=>
+{
+  const interval = setInterval(()=>{
+  fetch("http://localhost:5000/api").then(response =>response.json()).then(
+user=>
+    setData(user)
+  )},2000
+)
+
+return  ()=> clearInterval(interval)
+},[]);
+
   return (
     <div>
-      Hello
+     {
+
+      data.length ===0 ?
+     <p>Loading</p> :
+      data.map((names)=>
+      {
+       
+        return <p>{names}</p>
+      })
+     }
     </div>
   );
 }
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:"GET"
+}))
 
+
+app.get("/api",(req,res)=>{
+
+    res.send(["moon","bilal","Saad"])
+})
 app.listen("5000",(req,res)=>{
 
     console.log("Server running on port 5000");
